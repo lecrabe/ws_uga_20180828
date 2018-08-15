2308ec383c7a169a546ee8e3504c6b79dbd7e997
lecrabe <remi.dannunzio@gmail.com>
2018-08-15 18:23:14 +0200

    first

---
+++ README.md
@@ -0,0 +1,24 @@
+### Workshop on SEPAL, presentations & scripts repository
+The material on this repo has been developed for a workshop in Uganda from 27 to 31 August 2018.
+
+It contains: 
+
+- theoretical background on the BFAST algorithm developed by Wageningen University and the underlying concepts of dense time series analysis
+
+- practical tutorials to the use of the bfastSpatial package within SEPAL 
+
+- practical tutorials to the use of R modules to process global data (ESA CCI, GFC) as proxies for activity data
+
+- practical tutorials to the production of time series snippets to be used as complementary reference data for accuracy assessment and change area estimation
+
+### Requirements for the workshop
+
+If you do not have access to SEPAL, follow the below instructions:
+
+##### 1/ get a GMAIL account (in order to access Google Earth Engine functionalities)
+
+##### 2/ get the account registered and white listed in Google Earth Engine https://earthengine.google.com/signup/
+
+##### 3/ open an account in SEPAL https://tinyurl.com/sepal-access
+
+Access to SEPAL @ https://sepal.io
---
+++ scripts/s1_get_gadm_data.R
@@ -0,0 +1,63 @@
+####################################################################################################
+####################################################################################################
+## Get GADM data
+## Contact remi.dannunzio@fao.org 
+## 2018/08/15
+####################################################################################################
+####################################################################################################
+
+####################################################################################################
+options(stringsAsFactors = FALSE)
+
+### Load necessary packages
+library(raster)
+library(rgeos)
+library(ggplot2)
+library(rgdal)
+
+## Set the working directory
+
+## Get the list of countries and select one
+(gadm_list  <- data.frame(getData('ISO3')))
+listcodes   <- "UGA"
+countrycode <- listcodes[1]
+
+## Get GADM data and export as shapefile
+aoi         <- getData('GADM',path=gadm_dir , country= countrycode, level=1)
+writeOGR(aoi,
+         paste0(gadm_dir,"gadm_",countrycode,"_l1.shp"),
+         paste0("gadm_",countrycode,"_l1"),
+         "ESRI Shapefile",
+         overwrite_layer = T)
+
+## Display the shapefile
+plot(aoi)
+
+## Make a copy, subset the columns to 3 and export as KML
+aoi_kml      <- aoi
+aoi_kml@data <- aoi_kml@data[,c("OBJECTID","ISO","NAME_1")]
+
+writeOGR(aoi_kml,
+         paste0(gadm_dir,"gadm_",countrycode,".kml"),
+         paste0("gadm_",countrycode),
+         "KML",
+         overwrite_layer = T)
+
+## Make a copy, subset the columns to 3 and export as KML
+levels(as.factor(aoi$NAME_1))
+
+
+## Select one province and export as KML
+sub_aoi <- aoi[aoi$NAME_1 == "Kampala",]
+
+## Display the sub-shapefile on top of the previous one, in RED
+plot(sub_aoi,
+     add=T,
+     col="red")
+
+## Subset the columns to 2 and export as Shapefile
+sub_aoi@data <- sub_aoi@data[,c("OBJECTID","ISO")]
+
+writeOGR(sub_aoi,paste0(gadm_dir,"work_aoi_sub.kml"),"work_aoi_sub","KML",overwrite_layer = T)
+writeOGR(sub_aoi,paste0(gadm_dir,"work_aoi_sub.shp"),"work_aoi_sub","ESRI Shapefile",overwrite_layer = T)
+
