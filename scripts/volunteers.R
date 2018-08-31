61247d81e85915049b0a9a844e18c5afe86d6459
lecrabe <remi.dannunzio@gmail.com>
2018-08-31 13:03:44 +0300

    volunteers

---
+++ scripts/volunteers.R
@@ -0,0 +1,11 @@
+we_are <- c("margaret","ivan","brenda","praise","john_d","edward","fridah","john_b","deo","kissa","maria","enock","fidel")
+sample(we_are,1)
+
+mood <- "excited or disappointed"
+adjective <- "grateful"
+
+sprintf("I feel %s and very %s still",
+        mood,
+        adjective)
+
+### Fusion table ID for CFR BFAST analysis:   1LYUeeeKT0RQ-1dQfInI64M5nWx_rYLCkfH1GXBp2
